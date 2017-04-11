ea4d1a86ee4e1eec7b43fe0839109db791bc6e78
Mateusz Sienkan <sin.mateusz@gmail.com>
2017-04-11 22:28:27 +0200

    Move modules to separate directory

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo 'Updating modules...'
 git submodule update --init --recursive
 
 echo 'Installing fonts...'
-fonts/install.sh
+submodules/fonts/install.sh
 
 echo 'Installing Xcode command line tools...'
 xcode-select --install
@@ -11,9 +11,13 @@ echo 'Setting zsh as default shell...'
 chsh -s /bin/zsh
 
 echo 'Creating symlinks...'
-ln -shf $PWD/mjolnir-config $HOME/.mjolnir
-ln -shf $PWD/oh-my-zsh $HOME/.oh-my-zsh
+
+export MODULES=$PWD/modules
+
+ln -shf $MODULES/mjolnir-config $HOME/.mjolnir
+ln -shf $MODULES/oh-my-zsh $HOME/.oh-my-zsh
 ln -shf $PWD/.tmux.conf $HOME/.tmux.conf
-ln -shf $PWD/.zshrc $HOME/.zshrc
+ln -shf $PWD/.zshrc $HOME/.zshrc 
 
 echo 'Done!'
+
